193dfc78959c5c64138813841a7106ce3aa34b2a
Leon-Parepko <l.parepko@innopolis.university>
2022-02-13 05:32:12 +0600

    add hash check, array reform and loop all (also add some ad closers)

--- Main.py
+++ Main.py
@@ -4,26 +4,26 @@ from selenium import webdriver
 from pyvirtualdisplay import Display
 
 if __name__ == '__main__':
-    try:
+    # try:
         # Init Virtual Display (Used on Unix typed OS without GUI)
         # display = Display(visible=0, size=(1, 1))
         # display.start()
 
-        # Init Web Driver
-        driver = webdriver.Chrome()
-        # driver = webdriver.Firefox()
-        driver.maximize_window()
+    # Init Web Driver
+    driver = webdriver.Chrome()
+    # driver = webdriver.Firefox()
+    driver.maximize_window()
 
 
-        parser_func.login(driver)
-        parser_func.parse(driver)
+    parser_func.login(driver)
+    parser_func.parse(driver, 20)
 
 
 
 
-    except Exception as e:
-        print(e)
-    pass
+    # except Exception as e:
+    #     print(e)
+    # pass
 
     if driver:
         driver.close()
--- parser_func.py
+++ parser_func.py
@@ -11,11 +11,6 @@ from pyvirtualdisplay import Display
 
 
 
-# file_list = os.listdir(str(os.getcwd()) + "\Music_Output")
-# file_list = list(map(lambda x: x.split(".")[0].split("_"), file_list))
-print(hashing.hash([["slovo", "qweqwgbhkfdnlkkrdf02fm90"], ["qweqw", "qweqwgbhkfnle3kkdf02fm90"], ["qwaswdasd32r2fqw", "qwdfbdfbdfbm90"], ["qwfnhngfbfdeqw", "2fm9778680"], ["qweqw", "qw"], ["qweqweqwefdsgfdgqw", "qqwef02fm90"], ["qweqw", "qqqwxekdf02fm90"], ["qqweqw", "qweqwgbhkfdnlkkrdf02fm90"], ["qweqw", "qweqwgbhkfnle3kkdf02fm90"], ["qwaswdasd32r2fqw", "qwdfbdfbdfbm90"], ["qwfnhngfbfdeqw", "2fm9778680"], ["qweqw", "qw"], ["qweqweqwefdsgfdgqw", "qqwef02fm90"], ["qweqw", "qqqwxekdf02fm90"], ["qqweqw", "qweqwgbhkfdnlkkrdf02fm90"], ["qweqw", "qweqwgbhkfnle3kkdf02fm90"], ["qwaswdasd32r2fqw", "qwdfbdfbdfbm90"], ["qwfnhngfbfdeqw", "2fm9778680"], ["qweqw", "qw"], ["qweqweqwefdsgfdgqw", "qqwef02fm90"], ["qweqw", "qqqwxekdf02fm90"], ["qqweqw", "qweqwgbhkfdnlkkrdf02fm90"], ["qweqw", "qweqwgbhkfnle3kkdf02fm90"], ["qwaswdasd32r2fqw", "qwdfbdfbdfbm90"], ["qwfnhngfbfdeqw", "2fm9778680"], ["qweqw", "qw"], ["qweqweqwefdsgfdgqw", "qqwef02fm90"], ["qweqw", "qqqwxekdf02fm90"], ["qqweqw", "qweqwgbhkfdnlkkrdf02fm90"], ["qweqw", "qweqwgbhkfnle3kkdf02fm90"], ["qwaswdasd32r2fqw", "qwdfbdfbdfbm90"], ["qwfnhngfbfdeqw", "2fm9778680"], ["qweqw", "qw"], ["qweqweqwefdsgfdgqw", "qqwef02fm90"], ["qweqw", "qqqwxekdf02fm90"]]))
-# print(file_list)
-
 # display = Display(visible=0, size=(1, 1))
 # display.start()
 
@@ -41,52 +36,85 @@ def login(driver):
 
 
 
-def parse(driver):
-
-    split_symbol = "  #  "
-
-    href_arr = []
-    info_arr = []
+def parse(driver, delay):
 
     while True:
-        time.sleep(30)
+        split_symbol = "  #  "
 
-        soup = BeautifulSoup(driver.page_source, features="html.parser")
+        href_arr = []
+        info_arr = []
 
-        hrefs = soup.find_all("td", {"class": "align-middle pr-0 text-right"})
-        info = soup.find_all("td", {"class": "px-0"})
+        while True:
+            time.sleep(7)
 
-        href_arr += (list(map(lambda x: (f'https:{x.find_all("a")[0].get("href")}'), hrefs)))
-        info_arr += (list(map(lambda x: (x.findAll(text=True))[:3], info)))
+            soup = BeautifulSoup(driver.page_source, features="html.parser")
 
-        # print(href_arr)
-        # print(info_arr)
+            adds = driver.find_elements_by_class_name("close")
+            for elem in adds:
+                try:
+                    elem.click()
+                except:
+                    pass
 
-        next = driver.find_elements_by_css_selector(".btn.btn-link.text-decoration-none")
-        next[len(next) - 1].click()
-        try:
-            print(next[len(next) - 1].getAttribute("disabled"))
-            break
-        except:
-            pass
+            hrefs = soup.find_all("td", {"class": "align-middle pr-0 text-right"})
+            info = soup.find_all("td", {"class": "px-0"})
 
-    parse_list = list(map(lambda x: x[:2], info_arr))
+            href_arr += (list(map(lambda x: (f'https:{x.find_all("a")[0].get("href")}'), hrefs)))
+            info_arr += (list(map(lambda x: (x.findAll(text=True))[:3], info)))
 
-    file_list = os.listdir(str(os.getcwd()) + "\Music_Output")
-    file_list = list(map(lambda x: x.split(".")[0].split(split_symbol), file_list))
 
-    if hash_check(parse_list, file_list) == False:
+            try:
+                next = driver.find_elements_by_css_selector(".btn.btn-link.text-decoration-none")
+                next[len(next) - 1].click()
+            except:
 
-        index = 0
-        for item in parse_list:
-            if item not in file_list:
-                r = requests.get(href_arr[index], allow_redirects=True)
-                open(f'{str(item[0]) + split_symbol + str(item[1])}.mp3', 'wb').write(r.content)
-            index += 1
+                driver.find_element_by_xpath('//*[@id="kvk-header"]/div/a').click()
 
-        for item in file_list:
-            if item not in parse_list:
-                os.remove(f'{str(item[0]) + split_symbol + str(item[1])}.mp3')
+
+                # Test (close traceback add)
+                try:
+                    driver.find_element_by_class_name("ns-vnrx1-e-16").click()
+                except:
+                    pass
+
+
+                # print("Finished\n\n************************************")
+                # print(href_arr)
+                # print("\n\n************************************")
+                # print(info_arr)
+                break
+
+
+        parse_list = []
+        temp_href_arr = []
+        for i in range(0, len(info_arr)):
+            info_arr[i][0] = info_arr[i][0].strip()
+            info_arr[i][1] = info_arr[i][1].strip()
+
+            if (info_arr[i][:2] != ['Без названия', 'Неизвестен']):
+                parse_list.append(info_arr[i][:2])
+                temp_href_arr.append(href_arr[i])
+        href_arr = temp_href_arr
+
+
+        file_list = os.listdir(str(os.getcwd()) + "\Music_Output")
+        file_list = list(map(lambda x: x.split(".")[0].split(split_symbol), file_list))
+
+        if hash_check(parse_list, file_list) == False:
+            print("Loading files...")
+
+            index = 0
+            for item in parse_list:
+                if item not in file_list:
+                    r = requests.get(href_arr[index], allow_redirects=True)
+                    open(f'Music_Output\{str(item[0]) + split_symbol + str(item[1])}.mp3', 'wb').write(r.content)
+                index += 1
+
+            for item in file_list:
+                if item not in parse_list:
+                    os.remove(f'Music_Output\{str(item[0]) + split_symbol + str(item[1])}.mp3')
+        time.sleep(60 * delay)
+
 
 
 
@@ -97,10 +125,3 @@ def hash_check(info_arr_1, info_arr_2):
         return True
     else:
         return False
-
-
-
-def download_files():
-    # r = requests.get("https://i114.kissvk.com/api/song/download/get/11/King%20Gnu-Sakayume-kissvk.com.mp3?origin=kissvk.com&url=sid%3A%2F%2F306332812_456239846_5e3dd48a3f140ab34f_d046e9f1b38486eb97&artist=King%20Gnu&title=Sakayume&index=0&user_id=384615283&ui_version=220118202148&future_urls=sid%3A%2F%2F306332812_456239847_64be4b52c51f2a3cfd_6c65bbdf444e082385%2Csid%3A%2F%2F306332812_456239845_e7936891b0f4325470_75582aeb2f4f848517%2Csid%3A%2F%2F306332812_456239843_c5ee4da2467c4b1259_ae334b3217591dcb40%2Csid%3A%2F%2F306332812_456239842_2bc910fac7b065428e_511e822973f4f58c5a%2Csid%3A%2F%2F306332812_456239841_360e42723b94fde72e_9c9a6aa2c8065af1af%2Csid%3A%2F%2F306332812_456239840_25a0abe56e18b21652_3fa52f5b9b83397936%2Csid%3A%2F%2F306332812_456239839_102aef4bb658524cd9_c9b85f969c64b3f958%2Csid%3A%2F%2F306332812_456239838_38728285bbaf002eab_2e2b1531aa02b47685%2Csid%3A%2F%2F306332812_456239837_7ec7a5c732a7b4b4b7_ef677ccdc3b8997507", allow_redirects=True)
-    # open('test.mp3', 'wb').write(r.content)
-    pass
